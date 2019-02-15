5302813a4ac63f1175a71b011415afb3254459b2
rabbani <rabbani204>
2019-02-16 02:09:24 +0600

    update

---
+++ components/Tablee.vue
@@ -0,0 +1,41 @@
+<template>
+<table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Values:</th>
+        <a href="#"><th>Istanbul</th></a>
+        <a href="#"><th>Berlin</th></a>
+        <a href="#"><th>London</th></a>
+        <a href="#"><th>Helsinki</th></a>
+        <a href="#"><th>Dublin</th></a>
+        <a href="#"><th>Vancouver</th></a>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Temperature: </td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+            <tr>
+        <td>Max-Temp:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Min-Tem:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+      <Tablee />
+  </section>
+</template>
+
+<script>
+import Tablee from '~/components/Tablee.vue'
+
+export default {
+  components: {
+    Tablee
+  }
+}
+</script>
+
+<style>
+.container{
+  margin-top: 100px;
+  background-color: #E0E0E0;
+  padding: 125px;
+}
+</style>
+
